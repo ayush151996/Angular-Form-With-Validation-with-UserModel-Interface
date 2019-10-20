ef284240ca72566bad3e9f859fd9f4c6a03b508a
ayush151996 <ayured2011@gmail.com>
2019-10-20 20:15:27 +0530

    Angular Form With Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { User} from './user'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frms';
+  topics = ['Angular', 'React', 'Vue'];
+  userModel = new User('', 'rob@test.com', 5556665566, 'default', );
 }
